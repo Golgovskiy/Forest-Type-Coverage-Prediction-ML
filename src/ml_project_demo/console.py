f7e3ac301de11e54dfea78a7a4bd9fe65611b3c8
Golgovskiy Alexey <alexeygolgovskiy@gmail.com>
2022-05-07 19:14:27 +0300

    +pandas profiling +report generation

--- src/ml_project_demo/console.py
+++ src/ml_project_demo/console.py
@@ -1,10 +1,11 @@
 import click
 
 from . import __version__
-
+from data.report import makeReport
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """Menu"""
-    click.echo("Hello, world!")
+    #click.echo("Hello, world!")
+    makeReport()
