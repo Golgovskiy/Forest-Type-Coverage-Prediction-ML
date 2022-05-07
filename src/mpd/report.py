0a869af87b95f2f43f51ba51420c30db75d07743
Golgovskiy Alexey <alexeygolgovskiy@gmail.com>
2022-05-07 21:10:27 +0300

    *renamed the project to get rid of bug +added placeholder modules

---
+++ src/mpd/report.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import click
+import os
+
+from pandas_profiling import ProfileReport
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-to",
+    default="data/report.html",
+    type=click.Path(exists=False),
+    show_default=True,
+)
+def makeReport(dataset_path, save_to):
+    df = pd.read_csv(dataset_path)
+    profile = ProfileReport(df, title="Pandas Profiling Report", missing_diagrams={"Count": False})
+    profile.to_file(save_to)
+    click.echo("Report was saved to: " + save_to)
