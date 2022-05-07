08ce3f8dfea7c77f4bd6fbd23e60b89ab2479a66
Golgovskiy Alexey <alexeygolgovskiy@gmail.com>
2022-05-07 22:13:39 +0300

    +replace demo code with actual code for the homework v1

--- src/mpd/train.py
+++ src/mpd/train.py
@@ -5,7 +5,8 @@ import click
 # import mlflow
 # import mlflow.sklearn
 import sklearn
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,roc_auc_score,f1_score
+from sklearn.model_selection import cross_validate
 
 from .data import get_dataset
 from .pipeline import create_pipeline
@@ -15,7 +16,7 @@ from .pipeline import create_pipeline
 @click.option(
     "-d",
     "--dataset-path",
-    default="data/heart.csv",
+    default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
     show_default=True,
 )
@@ -33,9 +34,9 @@ from .pipeline import create_pipeline
     show_default=True,
 )
 @click.option(
-    "--test-split-ratio",
-    default=0.2,
-    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    "--k-folds",
+    default=5,
+    type=int,
     show_default=True,
 )
 @click.option(
@@ -60,29 +61,40 @@ def train(
     dataset_path: Path,
     save_model_path: Path,
     random_state: int,
-    test_split_ratio: float,
+    k_folds: int,
     use_scaler: bool,
     max_iter: int,
     logreg_c: float,
 ) -> None:
-    features_train, features_val, target_train, target_val = get_dataset(
+    features, target = get_dataset(
         dataset_path,
         random_state,
-        test_split_ratio,
+        k_folds,
     )
+    
+    if(k_folds <= 1 or k_folds >=len(features):
+        raise click.ClickException("Wrong k-folds value!")
+        
     with mlflow.start_run():
+    
         pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
         
-        pipeline.fit(features_train, target_train)
+        result = cross_validate(pipeline, features, target, scoring = ['f1','accuracy', 'roc_auc'], cv=k_folds)
         
-        accuracy = accuracy_score(target_val, pipeline.predict(features_val))
+        accuracy = np.mean(result["test_score"]["accuracy"])
+        f1 = np.mean(result["test_score"]["f1"])
+        roc_auc = np.mean(result["test_score"]["roc_auc"])
         
         mlflow.log_param("use_scaler", use_scaler)
         mlflow.log_param("max_iter", max_iter)
         mlflow.log_param("logreg_c", logreg_c)
-        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_param("k-folds", k-folds)
         
-        click.echo(f"Accuracy: {accuracy}.")
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("f1", f1)
+        mlflow.log_metric("roc_auc", roc_auc)
+        
+        click.echo(f"Accuracy: {accuracy}. F1: {f1}. ROC_AUC: {roc_auc}.")
         
         dump(pipeline, save_model_path)
         
